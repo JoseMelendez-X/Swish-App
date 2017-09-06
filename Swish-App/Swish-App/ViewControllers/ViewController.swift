50b00ac57fc81f4ba7c1f2a9fb5edadff414dae8
Jose Melendez <JoseM_Melendez@icloud.com>
2017-09-06 15:48:01 -0400

    Organized project, ready to begin

---
+++ Swish-App/Swish-App/ViewControllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Swish-App
+//
+//  Created by Jose Melendez on 9/6/17.
+//  Copyright © 2017 JoseMelendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
